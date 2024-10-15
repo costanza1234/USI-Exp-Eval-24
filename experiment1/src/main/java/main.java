5fa22eb5203f05398cd8fdf5c8dffaa7951f2348
costanza <rodric@usi.ch>
2024-10-15 11:13:33 +0200

    renamed Main

--- experiment1/src/main/java/main.java
+++ experiment1/src/main/java/main.java
@@ -1,6 +1,6 @@
 package experiment1.src.main.java;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Integer[] items = { 3, 2, 1 };
         Sorter<Integer> sorter = new SelectionSortGPT<>();
